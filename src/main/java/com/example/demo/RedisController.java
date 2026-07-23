f7e25588c6a133a9c798696a025733c5f58205cc
seokjun01 <suckjungod@naver.com>
2026-07-23 10:58:24 +0900

    test : TTL 적용

--- src/main/java/com/example/demo/RedisController.java
+++ src/main/java/com/example/demo/RedisController.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("/redis")
 @AllArgsConstructor
@@ -22,5 +25,17 @@ public class RedisController {
     public String get(@RequestParam String key) {
         String value = redisTemplate.opsForValue().get(key);
         return value != null ? value : "값 없음";
+    }
+
+    @PostMapping("/set/TTL")
+    public String setTTL (@RequestParam String key ,@RequestParam String value,@RequestParam int seconds) {
+        redisTemplate.opsForValue().set(key, value , Duration.ofSeconds(seconds));
+        return "저장 완료 : " + key + " = " + value + "유효 시간 " + seconds + "초";
+    }
+
+    @GetMapping("/get/TTL")
+    public String getTTL( @RequestParam String key) {
+        Long remainSeconds =redisTemplate.getExpire(key, TimeUnit.SECONDS);
+        return "남은 유효시간: " + remainSeconds + "초";
     }
 }
