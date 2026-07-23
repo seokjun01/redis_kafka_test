37896a35e81a0f91792ba34380de39f16b25cf74
seokjun01 <suckjungod@naver.com>
2026-07-23 10:29:12 +0900

    feat: postman요청, redis-cli 값 확인

--- src/main/java/com/example/demo/RedisController.java
+++ src/main/java/com/example/demo/RedisController.java
@@ -1,4 +1,26 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/redis")
+@AllArgsConstructor
 public class RedisController {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @PostMapping("/set")
+    public String set(@RequestParam String key,@RequestParam String value) {
+        redisTemplate.opsForValue().set(key,value);
+        return "저장 완료 : " + key + " = " + value;
+    }
+
+
+    @GetMapping("/get")
+    public String get(@RequestParam String key) {
+        String value = redisTemplate.opsForValue().get(key);
+        return value != null ? value : "값 없음";
+    }
 }
